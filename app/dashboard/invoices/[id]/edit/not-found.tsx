831b1e200c39405878b8aba97701f9f1c42927f0
Lucas Altenhofen <lucasaltenhofen@MacBook-Pro-de-Lucas.local>
2024-06-23 23:47:17 -0300

    adding metadata to dashboard app

--- app/dashboard/invoices/[id]/edit/not-found.tsx
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link';
 import { FaceFrownIcon } from '@heroicons/react/24/outline';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoice Not Found',
+};
+
 export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
